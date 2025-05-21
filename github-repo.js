6066b09bad0d3e0d2387fd703de7a3a9d0d65e66
rice/alex <noctiicus@gmail.com>
2025-05-21 15:03:30 -0700

    Update github-repo.js

--- github-repo.js
+++ github-repo.js
@@ -1,15 +1,14 @@
 // for if you wanna use your own files
 (function () {
-  const scriptTag = document.currentScript;
-  const username = scriptTag.getAttribute("data-username");
-  const containerId = scriptTag.getAttribute("data-container") || "github-repos";
+  const username = "USERNAME HERE";
+  const containerId = "github-repos";
   const container = document.getElementById(containerId);
   if (!container || !username) return;
 
   fetch(`https://api.github.com/users/${username}`)
     .then(res => res.json())
     .then(user => {
-      const avatarUrl = user.avatar_url + "&s=48";
+      const avatarUrl = user.avatar_url + "&s=40";
 
       fetch(`https://api.github.com/users/${username}/repos`)
         .then(res => res.json())
@@ -18,7 +17,7 @@
             const box = document.createElement("div");
             box.className = "repo-box";
             box.innerHTML = `
-              <a class="redirect" href="${repo.html_url}" target="_blank"><i class="fas fa-external-link-alt"></i></a>
+              <a class="redirect" href="${repo.html_url}" target="_blank" title="View on GitHub"><i class="fas fa-external-link-alt"></i></a>
               <div class="repo-header">
                 <img src="${avatarUrl}" alt="${username} avatar">
                 <a href="${repo.html_url}" target="_blank">${username} / ${repo.name}</a>
